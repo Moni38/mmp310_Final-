de196bfb2a30d85b026ae53d70fa6def9d0a0fb3
Moni38 <42787726+Moni38@users.noreply.github.com>
2019-05-11 16:53:37 -0400

    added working files

---
+++ libraries/particle.js
@@ -0,0 +1,31 @@
+var Particle = function(){
+	this.xPosition = 0;
+	this.yPosition = random(height);
+	this.xSpeed = random(6,10);
+	this.ySpeed = random(-5,5);
+	this.lifeSpan = 1000;
+
+	this.update = function(){
+		this.xPosition += this.xSpeed;
+		this.yPosition += this.ySpeed;
+		this.lifeSpan --;
+	}
+	this.display = function(){		
+		push();
+			noStroke();
+			fill(255, 45);
+			translate(this.xPosition,this.yPosition);
+
+			var scaleFactor = map(this.lifeSpan, 500,0, 1,0);
+			scale(scaleFactor);
+			ellipse(0,0,5);
+		pop();
+	}
+	this.checkDead = function(){
+		if (this.lifeSpan < 1){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,55 @@
+let bgImg;
+var x1;
+var x2;
+var scrollSpeed = 1.3;
+var clouds = [];
+var song;
+var soundFormats;
+
+function preload() {
+    soundFormats('mp3', 'ogg');
+    song = loadSound("sounds/christmas.mp3");
+
+    bgImg = loadImage("images/background-image.jpg");
+    //mySong = loadSound("sounds/christmas.mp3"); // loads music
+
+}
+
+function setup() {
+    createCanvas(1300, 558);
+
+    x1 = 0;
+    x2 = width;
+
+    song.setVolume(0.1);
+    song.play();
+
+}
+
+function draw() {
+
+    //This will make the background scroll endlessly
+    image(bgImg, x1, 0, width, height);
+    image(bgImg, x2, 0, width, height);
+
+    x1 -= scrollSpeed;
+    x2 -= scrollSpeed;
+
+    if (x1 < -width) {
+        x1 = width;
+    }
+    if (x2 < -width) {
+        x2 = width;
+    }
+
+}
+
+
+function mousePressed() {
+    // trigger sound
+    song.play();
+
+    // change background color
+    background(random(255), random(255), random(255));
+
+}
